86dc4daea0cee0f36964327ae8b50aceeef3921a
Daniel <danielgulic@gmail.com>
2021-08-03 04:05:34 +1000

    Add Database connection to vcs!

---
+++ src/main/kotlin/database/Database.kt
@@ -0,0 +1,21 @@
+package network.warzone.api.database
+
+import network.warzone.api.database.model.Player
+import network.warzone.api.database.model.Session
+import org.litote.kmongo.coroutine.CoroutineCollection
+import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+object Database {
+    private val database: CoroutineDatabase;
+    val players: CoroutineCollection<Player>
+    val sessions: CoroutineCollection<Session>
+
+    init {
+        val client = KMongo.createClient().coroutine;
+        database = client.getDatabase("warzone-api")
+        players = database.getCollection()
+        sessions = database.getCollection()
+    }
+}
