9ce44b370ac4895d481179655836fa3027d27e39
jorgeberrex <jorgeberrex@hotmail.com>
2022-01-27 15:10:55 -0500

    Broadcasts use names instead of IDs

--- src/main/kotlin/database/models/Broadcast.kt
+++ src/main/kotlin/database/models/Broadcast.kt
@@ -3,4 +3,4 @@ package network.warzone.api.database.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Broadcast(val id: String, val message: String, val permission: String? = null, val newline: Boolean = true)
+data class Broadcast(val name: String, val message: String, val permission: String? = null, val newline: Boolean = true)
