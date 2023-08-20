159e1aef9f3d3abc73903df7c403ceb011cac99b
Mew2K <MewTwoKing5@gmail.com>
2023-08-19 20:43:40 -0500

    Validation Patch - Fixed missing validations for achievement routes.

--- src/main/kotlin/http/achievements/AchievementRoutes.kt
+++ src/main/kotlin/http/achievements/AchievementRoutes.kt
@@ -12,48 +12,66 @@ import network.warzone.api.http.AchievementMissingException
 import network.warzone.api.http.RankMissingException
 import network.warzone.api.http.ValidationException
 import network.warzone.api.util.capitalizeFirst
+import network.warzone.api.util.protected
+import network.warzone.api.util.validate
 import java.util.*
 
 fun Route.addAchievement() {
     post {
-        val newAchievementRequest = call.receive<AchievementCreateRequest>()
-        val newAchievement = Achievement(
-            _id = UUID.randomUUID().toString(),
-            name = newAchievementRequest.name,
-            description = newAchievementRequest.description,
-            parent = newAchievementRequest.parent,
-            agent = newAchievementRequest.agent
-        )
-        val resultId = Achievement.addAchievement(newAchievement)
-        call.respond(HttpStatusCode.Created, resultId)
+        protected(this) { _ ->
+            validate<AchievementCreateRequest>(this) { data ->
+                val newAchievementRequest = call.receive<AchievementCreateRequest>()
+                val newAchievement = Achievement(
+                    _id = UUID.randomUUID().toString(),
+                    name = newAchievementRequest.name,
+                    description = newAchievementRequest.description,
+                    parent = newAchievementRequest.parent,
+                    agent = newAchievementRequest.agent
+                )
+                val resultId = Achievement.addAchievement(newAchievement)
+                call.respond(HttpStatusCode.Created, resultId)
+            }
+        }
     }
 }
 
 fun Route.getAchievements() {
     get {
-        val achievements = Achievement.getAchievements()
-        call.respond(HttpStatusCode.OK, achievements)
+        protected(this) { _ ->
+            validate<AchievementCreateRequest>(this) { data ->
+                val achievements = Achievement.getAchievements()
+                call.respond(HttpStatusCode.OK, achievements)
+            }
+        }
     }
     get("/{achievementId}") {
-        val id = call.parameters["achievementId"] ?: throw ValidationException()
-        val achievement = Database.achievements.findByIdOrName(id) ?: throw AchievementMissingException()
-        call.respond(achievement)
+        protected(this) { _ ->
+            validate<AchievementCreateRequest>(this) { data ->
+                val id = call.parameters["achievementId"] ?: throw ValidationException()
+                val achievement = Database.achievements.findByIdOrName(id) ?: throw AchievementMissingException()
+                call.respond(achievement)
+            }
+        }
     }
 }
 
 fun Route.deleteAchievement() {
     delete("/{id}") {
-        val id = call.parameters["id"]
-        if (id == null) {
-            call.respond(HttpStatusCode.BadRequest, "Missing or malformed id")
-            return@delete
-        }
+        protected(this) { _ ->
+            validate<AchievementCreateRequest>(this) { data ->
+                val id = call.parameters["id"]
+                if (id == null) {
+                    call.respond(HttpStatusCode.BadRequest, "Missing or malformed id")
+                    return@protected
+                }
 
-        val isDeleted = Achievement.deleteAchievement(id)
-        if (isDeleted) {
-            call.respond(HttpStatusCode.OK, "Achievement $id deleted successfully")
-        } else {
-            call.respond(HttpStatusCode.NotFound, "Achievement $id not found")
+                val isDeleted = Achievement.deleteAchievement(id)
+                if (isDeleted) {
+                    call.respond(HttpStatusCode.OK, "Achievement $id deleted successfully")
+                } else {
+                    call.respond(HttpStatusCode.NotFound, "Achievement $id not found")
+                }
+            }
         }
     }
 }
